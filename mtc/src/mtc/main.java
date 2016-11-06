e401b3bac0f1341ab93030ede781484724360a80
Chunheng Luo <chunhengluo@outlook.com>
2016-11-05 21:51:18 -0500

    Average time for each algorithm. Some comments about each algorithm.

--- mtc/src/mtc/main.java
+++ mtc/src/mtc/main.java
@@ -18,7 +18,7 @@ public class main {
 		public void run(){
 			for(int i = 0; i < times; i++){
 				// System.out.println("a " + hs.add(i));
-				hs.add(ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE)); 
+				hs.add(ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE)); 
 			}
 		}
 	}
@@ -36,7 +36,7 @@ public class main {
 		public void run(){
 			for(int i = 0; i < times; i++){
 				// System.out.println("r " + hs.remove(i));
-				hs.remove(ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE)); 
+				hs.remove(ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE)); 
 			}
 		}
 	}
@@ -54,7 +54,7 @@ public class main {
 		public void run(){
 			for(int i = 0; i < times; i++){
 				// System.out.println("c " + hs.contains(i));
-				hs.contains(ThreadLocalRandom.current().nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE)); 
+				hs.contains(ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE)); 
 			}
 		}
 	}
@@ -64,87 +64,94 @@ public class main {
 		/*
 		 * This part used to set the testing parameter as you like
 		 */
-		int num_of_addthread = 1;
-		int num_per_add = 100;
+		int num_of_addthread = 9;
+		int num_per_add = 1000;
 		int num_of_removethread = 1;
-		int num_per_remove = 100;
-		int num_of_containsthread = 1;
-		int num_per_contains = 100;
+		int num_per_remove = 1000;
+		int num_of_containsthread = 90;
+		int num_per_contains = 1000;
 		
 		int size_of_hashset = 1000;
+		int max_num_buckets = 100000; 
+		
+		int runs_per_algorithm = 5;  
 		
 		/*
 		 * int curHashSetIndex; 
 		 * 
 		 * Index for hashset in hashsetList:
-		 * 0. LockFreeHashSet
-		 * 1. CoarseHashSet
-		 * 2. RefinableHashSet
-		 * 3. StripedHashSet
-		 * 4. TCuckooHashSet
-		 * 5. StripedCuckooHashSet
-		 * 6. RefinableCuckooHashSet
-		 * 7. CoarseCuckooHashSet
-		 * 8. RWStripedHashSet
+		 * 0. LockFreeHashSet              Lock-free, split ordered 
+		 * 1. CoarseHashSet                Lock-based, coarse grained 
+		 * 2. StripedHashSet               Lock-based, fine grained, lock array unresizable
+		 * 3. RWStripedHashSet             Lock-based, fine grained, lock array unresizable, using R/W lock for resizing
+		 * 4. RefinableHashSet             Lock-based, fine grained, lock array resizable 
+		 * 5. StripedCuckooHashSet         Lock-based, fine grained, lock array unresizable, Cuckoo hashing 
+		 * 6. TCuckooHashSet               Lock-based, fine grained, lock array unresizable, Cuckoo hashing 
+		 * 7. RefinableCuckooHashSet       Lock-based, fine grained, lock array resizable, Cuckoo hashing
+		 * 8. ConcurrentHopscotchHashMap   (TODO) 
 		 */
 		ArrayList<Set<Integer>> hashsetList = new ArrayList<Set<Integer>>();
-		hashsetList.add(new LockFreeHashSet<Integer>(size_of_hashset));
+		hashsetList.add(new LockFreeHashSet<Integer>(max_num_buckets));
 		hashsetList.add(new CoarseHashSet<Integer>(size_of_hashset));
-		hashsetList.add(new RefinableHashSet<Integer>(size_of_hashset));
 		hashsetList.add(new StripedHashSet<Integer>(size_of_hashset));
-		hashsetList.add(new TCuckooHashSet<Integer>(size_of_hashset));
-		hashsetList.add(new StripedCuckooHashSet<Integer>(size_of_hashset));
-		hashsetList.add(new RefinableCuckooHashSet<Integer>(size_of_hashset));
-		hashsetList.add(new CoarseCuckooHashSet<Integer>(size_of_hashset));
 		hashsetList.add(new RWStripedHashSet<Integer>(size_of_hashset));
+		hashsetList.add(new RefinableHashSet<Integer>(size_of_hashset));
+		hashsetList.add(new StripedCuckooHashSet<Integer>(size_of_hashset));
+		hashsetList.add(new TCuckooHashSet<Integer>(size_of_hashset));
+		hashsetList.add(new RefinableCuckooHashSet<Integer>(size_of_hashset));
+		
 		
 		/*
 		 * threadpool for synchronization
 		 */
-		for (int curHashSetIndex = 0; curHashSetIndex < 9; curHashSetIndex += 1) {
-			ArrayList<Thread> threadPool = new ArrayList<Thread>();
-			Thread curThread;
+		for (int curHashSetIndex = 0; curHashSetIndex < hashsetList.size(); curHashSetIndex += 1) {
+			long totalTime = 0; 
+			for (int curRun = 0; curRun < runs_per_algorithm; curRun += 1) {
+				ArrayList<Thread> threadPool = new ArrayList<Thread>();
+				Thread curThread;
 
-			long startTime = System.nanoTime(); 
-			// add thread
-			for(int i = 0; i < num_of_addthread; i++){
-				curThread = new addThread(num_per_add, hashsetList.get(curHashSetIndex));
-				threadPool.add(curThread);
-				curThread.start();
+				long startTime = System.nanoTime(); 
+				// add thread
+				for(int i = 0; i < num_of_addthread; i++){
+					curThread = new addThread(num_per_add, hashsetList.get(curHashSetIndex));
+					threadPool.add(curThread);
+					curThread.start();
+				}
+				//remove thread
+				for(int i = 0; i < num_of_removethread; i++){
+					curThread = new removeThread(num_per_remove, hashsetList.get(curHashSetIndex));
+					threadPool.add(curThread);
+					curThread.start();
+				}
+				//contains thread
+				for(int i = 0; i < num_of_containsthread; i++){
+					curThread = new containsThread(num_per_contains, hashsetList.get(curHashSetIndex));
+					threadPool.add(curThread);
+					curThread.start();
+				}
+				// synchronization
+				for(Thread t: threadPool){
+					t.join();
+				}
+
+				// time calculation
+				long estimatedTime = System.nanoTime() - startTime;
+				totalTime += estimatedTime; 
 			}
-			//remove thread
-			for(int i = 0; i < num_of_removethread; i++){
-				curThread = new removeThread(num_per_remove, hashsetList.get(curHashSetIndex));
-				threadPool.add(curThread);
-				curThread.start();
-			}
-			//contains thread
-			for(int i = 0; i < num_of_containsthread; i++){
-				curThread = new containsThread(num_per_contains, hashsetList.get(curHashSetIndex));
-				threadPool.add(curThread);
-				curThread.start();
-			}
-			// synchronization
-			for(Thread t: threadPool){
-				t.join();
-			}
-			
+
 			// Display the current hashing algorithm 
 			switch (curHashSetIndex) {
 			case 0: System.out.println("LockFreeHashSet -----------------------------"); break; 
 			case 1: System.out.println("CoarseHashSet -------------------------------"); break; 
-			case 2: System.out.println("RefinableHashSet ----------------------------"); break; 
-			case 3: System.out.println("StripedHashSet ------------------------------"); break; 
-			case 4: System.out.println("TCuckooHashSet ------------------------------"); break; 
+			case 2: System.out.println("StripedHashSet ------------------------------"); break; 
+			case 3: System.out.println("RWStripedHashSet ----------------------------"); break; 
+			case 4: System.out.println("RefinableHashSet ----------------------------"); break; 
 			case 5: System.out.println("StripedCuckooHashSet ------------------------"); break; 
-			case 6: System.out.println("RefinableCuckooHashSet ----------------------"); break; 
-			case 7: System.out.println("CoarseCuckooHashSet -------------------------"); break; 
-			case 8: System.out.println("RWStripedHashSet ----------------------------"); break; 
+			case 6: System.out.println("TCuckooHashSet ------------------------------"); break; 
+			case 7: System.out.println("RefinableCuckooHashSet ----------------------"); break; 
 			}
-
-			// time calculation
-			long estimatedTime = System.nanoTime() - startTime;
-			System.out.println("- Time Used: " + estimatedTime/1000000 + " Milliseconds");
+			
+			System.out.println("- Time Used: " + (double)totalTime/runs_per_algorithm/1000000 + " Milliseconds");
 		}
     }
 }
